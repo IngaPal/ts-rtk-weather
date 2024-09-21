12e600a0efa109884bc38727eac94c781e7d6c2c
Inga Pal <IngaPUFFY@web.de>
2024-09-21 12:47:19 +0200

    correction commit

--- src/features/slices/weatherSlice.ts
+++ src/features/slices/weatherSlice.ts
@@ -1,42 +1,34 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { WeatherInfo } from "../../utils/types";
 import { base_url, api_key } from '../../utils/constant';
 import { putMessage } from './messageSlice';
-
-export const fetchWeather = createAsyncThunk<WeatherInfo, string>(
-    'weather/fetchWeather',
-    async (city, { dispatch }) => {
-    try {
-        const response = await fetch(`${base_url}?q=${city}&appid=${api_key}&units=metric`);
-        const data = await response.json();
-        const info: WeatherInfo = {
-        city: data.name,
-        country: data.sys.country,
-        temp: data.main.temp,
-        pressure: data.main.pressure,
-        sunset: data.sys.sunset
-    };
-        dispatch(putMessage(''));
-        return info;
-    } catch (e) {
-        dispatch(putMessage('Enter correct city name'));
-        throw e;
-    }
-}
-);
+import { AppDispatch } from "../../app/store";
 
 const weatherSlice = createSlice({
     name: 'weather',
     initialState: {} as WeatherInfo,
     reducers: {
-        putWeatherInfo: (_state, action) => action.payload
-    },
-    extraReducers: (builder) => {
-        builder.addCase(fetchWeather.fulfilled, (state, action) => {
-            return action.payload;
-        });
+        putWeatherInfo: (_state, action: PayloadAction<WeatherInfo>) => action.payload
     }
 });
 
 export const { putWeatherInfo } = weatherSlice.actions;
+export const fetchWeather = (city: string) => async (dispatch: AppDispatch) => {
+    try {
+        const response = await fetch(`${base_url}?q=${city}&appid=${api_key}&units=metric`);
+        const data = await response.json();
+        const info: WeatherInfo = {
+            city: data.name,
+            country: data.sys.country,
+            temp: data.main.temp,
+            pressure: data.main.pressure,
+            sunset: data.sys.sunset
+        };
+        dispatch(putWeatherInfo(info));
+        dispatch(putMessage(''));
+    } catch (e) {
+        dispatch(putMessage('Enter correct city name'));
+    }
+};
+
 export default weatherSlice.reducer;
